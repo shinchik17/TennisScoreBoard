82d14f23cbf48963cd91ab231861f3b77adf76e1
shinchik17 <shinyaev.2013@mail.ru>
2024-07-30 12:53:44 +0300

    feat: mapper

---
+++ src/main/java/com/alexshin/tennisscoreboard/mapper/MatchMapper.java
@@ -0,0 +1,58 @@
+package com.alexshin.tennisscoreboard.mapper;
+
+import com.alexshin.tennisscoreboard.model.MatchScoreModel;
+import com.alexshin.tennisscoreboard.model.dto.MatchDTO;
+import com.alexshin.tennisscoreboard.model.entity.Match;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+
+import static com.alexshin.tennisscoreboard.service.MatchScoreCalculationService.getPlayerPointString;
+
+
+public class MatchMapper extends ModelMapper {
+    private final ModelMapper mapper = new ModelMapper();
+
+
+    public MatchMapper() {
+        configureMappings();
+    }
+
+    private void configureMappings() {
+
+        Converter<MatchDTO, MatchScoreModel> toScoreConverter = context -> {
+            MatchDTO source = context.getSource();
+            return new MatchScoreModel(
+                    source.getPlayer1().getName(),
+                    source.getPlayer2().getName(),
+                    String.valueOf(source.getPlayer1Set()),
+                    String.valueOf(source.getPlayer2Set()),
+                    String.valueOf(source.getPlayer1Game()),
+                    String.valueOf(source.getPlayer2Game()),
+                    getPlayerPointString(source, 1),
+                    getPlayerPointString(source, 2),
+                    source.getWinner().getName()
+            );
+        };
+
+        TypeMap<MatchDTO, MatchScoreModel> dtoToScoreMap = mapper.createTypeMap(MatchDTO.class, MatchScoreModel.class);
+        dtoToScoreMap.setConverter(toScoreConverter);
+
+    }
+
+    public MatchScoreModel toScoreModel(MatchDTO match) {
+        return mapper.map(match, MatchScoreModel.class);
+    }
+
+
+    public MatchDTO toDTO(Match matchEntity) {
+        return mapper.map(matchEntity, MatchDTO.class);
+    }
+
+    public Match toEntity(MatchDTO match) {
+        return mapper.map(match, Match.class);
+    }
+
+}
+
+
