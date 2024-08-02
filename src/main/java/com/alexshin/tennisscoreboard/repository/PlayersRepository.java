79309c411b329c59c03118864a89f8fce754a2ac
shinchik17 <shinyaev.2013@mail.ru>
2024-08-02 13:01:19 +0300

    ref: change @Cleanup to try-catch blocks and add transaction rollback

--- src/main/java/com/alexshin/tennisscoreboard/repository/PlayersRepository.java
+++ src/main/java/com/alexshin/tennisscoreboard/repository/PlayersRepository.java
@@ -1,61 +1,94 @@
 package com.alexshin.tennisscoreboard.repository;
 
 
-
 import com.alexshin.tennisscoreboard.model.entity.Player;
 import com.alexshin.tennisscoreboard.util.HibernateUtil;
 import lombok.Cleanup;
 import org.hibernate.SessionFactory;
-import org.hibernate.exception.ConstraintViolationException;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.Optional;
 
 
-public class PlayersRepository implements Repository<Player>{
+public class PlayersRepository implements Repository<Player> {
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
     @Override
     public Player save(Player entity) {
-        @Cleanup var session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.persist(entity);
-        session.getTransaction().commit();
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
         return entity;
     }
 
     @Override
     public void delete(Player entity) {
-        @Cleanup var session = sessionFactory.openSession();
-        session.remove(entity);
-        session.flush();
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(Player entity) {
-        @Cleanup var session = sessionFactory.openSession();
-        session.merge(entity);
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Player> findById(Integer id) {
-        @Cleanup var session = sessionFactory.openSession();
-        return Optional.of(session.find(Player.class, id));
+        Optional<Player> result;
+        try (var session = sessionFactory.openSession()) {
+            result = Optional.of(session.find(Player.class, id));
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
     public Optional<Player> findByName(Player entity) {
-        @Cleanup var session = sessionFactory.openSession();
-        Query<Player> query = session.createQuery("FROM Player WHERE name = :name", Player.class);
-        query.setParameter("name", entity.getName());
-        query.uniqueResultOptional();
-        return query.uniqueResultOptional();
+        Optional<Player> result;
+        try (var session = sessionFactory.openSession()) {
+            Query<Player> query = session.createQuery("FROM Player WHERE name = :name", Player.class);
+            query.setParameter("name", entity.getName());
+            query.uniqueResultOptional();
+            result = query.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
 
-    public Player saveOrGet(Player entity){
+    public Player saveOrGet(Player entity) {
         var optEntity = findByName(entity);
         return optEntity.orElseGet(() -> save(entity));
-
     }
 
 
